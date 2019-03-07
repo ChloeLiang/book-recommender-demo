18b279a4bd5cdda0b7139529fb53586592a59781
ChloeLiang <nliangxin@gmail.com>
2019-03-07 19:52:28 +0800

    make nav bar sticky and change color

--- src/layouts/Layout/Layout.js
+++ src/layouts/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from '../../components/Navigation';
+import style from './Layout.module.scss';
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navigation />
-      <main>{children}</main>
+      <main className={style.mainContainer}>{children}</main>
     </div>
   );
 }
